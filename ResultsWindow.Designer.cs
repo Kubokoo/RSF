70aa860fce1063c49e361fd21a2fecb245aefea4
Kubokoo <Kubokoo@users.noreply.github.com>
2018-03-16 00:31:53 +0100

    Small chnages to allow window resising and lower ram usage

--- ResultsWindow.Designer.cs
+++ ResultsWindow.Designer.cs
@@ -28,131 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.TextBoxLeft = new System.Windows.Forms.RichTextBox();
-            this.TextBoxRight = new System.Windows.Forms.RichTextBox();
-            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.TextBoxLeft = new System.Windows.Forms.TextBox();
             this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.TextBoxRight = new System.Windows.Forms.TextBox();
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.buttonNext = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // splitContainer1
             // 
-            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel1.ColumnCount = 5;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.65693F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.34306F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 211F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 252F));
-            this.tableLayoutPanel1.Controls.Add(this.TextBoxLeft, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.TextBoxRight, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBoxRight, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBoxLeft, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(-1, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.38983F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.61017F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(593, 377);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonPrevious);
+            this.splitContainer1.Panel1.Controls.Add(this.TextBoxLeft);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBoxLeft);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonNext);
+            this.splitContainer1.Panel2.Controls.Add(this.TextBoxRight);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBoxRight);
+            this.splitContainer1.Size = new System.Drawing.Size(600, 378);
+            this.splitContainer1.SplitterDistance = 300;
+            this.splitContainer1.SplitterWidth = 2;
+            this.splitContainer1.TabIndex = 0;
             // 
             // TextBoxLeft
             // 
-            this.TextBoxLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.TextBoxLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel1.SetColumnSpan(this.TextBoxLeft, 2);
-            this.TextBoxLeft.Location = new System.Drawing.Point(3, 190);
+            this.TextBoxLeft.Location = new System.Drawing.Point(3, 211);
             this.TextBoxLeft.Name = "TextBoxLeft";
-            this.TextBoxLeft.Size = new System.Drawing.Size(286, 102);
-            this.TextBoxLeft.TabIndex = 0;
-            this.TextBoxLeft.Text = "";
-            // 
-            // TextBoxRight
-            // 
-            this.tableLayoutPanel1.SetColumnSpan(this.TextBoxRight, 2);
-            this.TextBoxRight.Location = new System.Drawing.Point(295, 190);
-            this.TextBoxRight.Name = "TextBoxRight";
-            this.TextBoxRight.Size = new System.Drawing.Size(286, 102);
-            this.TextBoxRight.TabIndex = 1;
-            this.TextBoxRight.Text = "";
-            // 
-            // pictureBoxRight
-            // 
-            this.pictureBoxRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxRight.BackColor = System.Drawing.SystemColors.Highlight;
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxRight, 2);
-            this.pictureBoxRight.Location = new System.Drawing.Point(295, 3);
-            this.pictureBoxRight.Name = "pictureBoxRight";
-            this.pictureBoxRight.Size = new System.Drawing.Size(286, 181);
-            this.pictureBoxRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxRight.TabIndex = 3;
-            this.pictureBoxRight.TabStop = false;
+            this.TextBoxLeft.Size = new System.Drawing.Size(294, 20);
+            this.TextBoxLeft.TabIndex = 1;
             // 
             // pictureBoxLeft
             // 
             this.pictureBoxLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxLeft.BackColor = System.Drawing.SystemColors.Highlight;
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxLeft, 2);
             this.pictureBoxLeft.Location = new System.Drawing.Point(3, 3);
             this.pictureBoxLeft.Name = "pictureBoxLeft";
-            this.pictureBoxLeft.Size = new System.Drawing.Size(286, 181);
+            this.pictureBoxLeft.Size = new System.Drawing.Size(294, 202);
             this.pictureBoxLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxLeft.TabIndex = 2;
+            this.pictureBoxLeft.TabIndex = 0;
             this.pictureBoxLeft.TabStop = false;
+            // 
+            // TextBoxRight
+            // 
+            this.TextBoxRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxRight.Location = new System.Drawing.Point(4, 211);
+            this.TextBoxRight.Name = "TextBoxRight";
+            this.TextBoxRight.Size = new System.Drawing.Size(296, 20);
+            this.TextBoxRight.TabIndex = 2;
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxRight.Location = new System.Drawing.Point(4, 0);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(296, 202);
+            this.pictureBoxRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRight.TabIndex = 1;
+            this.pictureBoxRight.TabStop = false;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(211, 343);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 3;
+            this.buttonNext.Text = "button1";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 298);
+            this.button2.Location = new System.Drawing.Point(130, 343);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
+            this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // buttonPrevious
             // 
-            this.button1.Location = new System.Drawing.Point(506, 298);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(425, 298);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Location = new System.Drawing.Point(3, 343);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 5;
+            this.buttonPrevious.Text = "button3";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(92, 298);
+            this.button4.Location = new System.Drawing.Point(84, 343);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 7;
+            this.button4.TabIndex = 6;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
@@ -160,27 +153,32 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 378);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(600, 378);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "ResultsWindow";
             this.Text = "Search Results";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.RichTextBox TextBoxLeft;
-        private System.Windows.Forms.RichTextBox TextBoxRight;
+        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.PictureBox pictureBoxLeft;
         private System.Windows.Forms.PictureBox pictureBoxRight;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox TextBoxLeft;
+        private System.Windows.Forms.TextBox TextBoxRight;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonNext;
     }
 }
--- ResultsWindow.cs
+++ ResultsWindow.cs
@@ -5,22 +5,45 @@ namespace RSF
 {
     public partial class ResultsWindow : Form
     {
+        int i = 0;
+
+        void ShowResults() //TODO USES TO MUCH RAM!!!!!!!
+        {
+            if(pictureBoxLeft.Image!=null)
+            {
+                pictureBoxLeft.Image.Dispose();
+                pictureBoxRight.Image.Dispose();
+            }
+            
+            if (RSF.repeatedImages.Count == i) i = 0;
+            if (i == -1) i = RSF.repeatedImages.Count - 1;
+
+
+            TextBoxLeft.Text = RSF.repeatedImages[i].filename.ToString() + RSF.repeatedImages[i].extension.ToString(); //TODO ADD EXEPTOIONS IF NOT PROCESSED (NO FILE TO SHOW)
+            Bitmap previewLeft = new Bitmap(RSF.repeatedImages[i].path);   //TODO Add bigger reolution for bigger window
+            pictureBoxLeft.Image = previewLeft;
+
+            TextBoxRight.Text = RSF.repeatedImages[i].repeatedWith.ToString();
+            Bitmap previewRight = new Bitmap(RSF.repeatedImages[i].repeatedWithPath); //TODO ADD PROPER RESIVE OF FIELDS WITH IMAGES
+            pictureBoxRight.Image = previewRight;
+        }
+
         public ResultsWindow()
         {
             InitializeComponent();
-            TextBoxLeft.Text = RSF.repeatedImages[0].filename.ToString() + RSF.repeatedImages[0].extension.ToString(); //TODO ADD EXEPTOIONS IF NOT PROCESSED (NO FILE TO SHOW)
-            Bitmap previewLeft = new Bitmap(Image.FromFile(RSF.repeatedImages[0].path), pictureBoxLeft.Size);   //TODO Add bigger reolution for bigger window
-            pictureBoxLeft.Image = previewLeft;
-
-            TextBoxRight.Text = RSF.repeatedImages[0].repeatedWith.ToString();
-            Bitmap previewRight = new Bitmap(Image.FromFile(RSF.repeatedImages[0].repeatedWithPath), pictureBoxLeft.Size); //TODO ADD PROPER RESIVE OF FIELDS WITH IMAGES
-            pictureBoxRight.Image = previewRight;
-
-            //for(int i = 0; i < RSF.repeatedImages.Count(); i++)
-            //{
-            //    TextBoxLeft.Text += RSF.repeatedImages[i].path.ToString();
-            //}
+            ShowResults();
         }
 
+        private void buttonNext_Click(object sender, System.EventArgs e)
+        {
+            i++;
+            ShowResults();
+        }
+
+        private void buttonPrevious_Click(object sender, System.EventArgs e)
+        {
+            i--;
+            ShowResults();
+        }
     }
 }
